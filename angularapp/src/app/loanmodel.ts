ce090eabba4bc2beaf2dc98aa8031a03200dc09a
pradeesh12 <pradeesh.m2018cse@sece.ac.in>
2022-03-21 20:10:04 +0530

    final_angular

---
+++ angularapp/src/app/loanmodel.ts
@@ -0,0 +1,14 @@
+export class Loanmodel {
+        loanId: number;
+		loantype: string;
+		applicantName: string;
+	    applicantAddress: string;
+		applicantMobile: string;
+		applicantEmail: string;
+		applicantAadharr: string;
+		applicantPan: string;
+		applicantSalary: string;
+		loanAmountRequired: string;
+		repaymentMonths: string;
+        apprej: string;
+}
